3113c4059309c1cc87c4aa90f63a39f4ccd0c7e4
段昊辰 <duanhaochen@126.com>
2018-12-15 00:36:22 +0800

    调整导航的样式

---
+++ src/assembly/Header/Nav.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'antd';
+import style from './style.sass';
+
+function Nav(props: Object): React.Element{
+  return (
+    <nav>
+      <ul className={ style.navList }>
+        <li>
+          <Link to="/">
+            <Icon type="home" theme="twoTone" />
+            <span>首页</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/CreateForm">
+            <Icon type="file-text" theme="twoTone" />
+            <span>表单生成</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <Icon type="eye" theme="twoTone" />
+            <span>表单预览</span>
+          </Link>
+        </li>
+        <li>
+          <a href="https://github.com/duan602728596/antd-schema-form#antd-schema-form"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="book" theme="twoTone" />
+            <span>文档</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/assembly/Header/index.js
+++ src/assembly/Header/index.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { Layout, Icon } from 'antd';
 import style from './style.sass';
+import Nav from './Nav';
 
 class Header extends Component{
   render(): React.Element{
     return (
       <Layout.Header>
-        <nav>
-          <ul className={ style.navList }>
-            <li>
-              <Link to="/">
-                <Icon type="home" theme="twoTone" />
-                <span>首页</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/CreateForm">
-                <Icon type="file-text" theme="twoTone" />
-                <span>表单生成</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <Icon type="eye" theme="twoTone" />
-                <span>表单预览</span>
-              </Link>
-            </li>
-            <li>
-              <a href="https://github.com/duan602728596/antd-schema-form#antd-schema-form"
-                target="_blank"
-                ref="noopener noreferrer"
-              >
-                <Icon type="book" theme="twoTone" />
-                <span>文档</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <div className={ style.headerBox }>
+          <Nav />
+          <a className={ style.github }
+            href="https://github.com/duan602728596/antd-schema-form"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="github" />
+          </a>
+        </div>
       </Layout.Header>
     );
   }
