2431f6dc17d00b29c537540c67a9f11f76dd8c32
段昊辰 <duanhaochen@126.com>
2018-12-15 10:28:04 +0800

    调整配置

--- .sweetrc.js
+++ .sweetrc.js
@@ -22,7 +22,7 @@ export default {
   entry: {
     app: [path.join(__dirname, 'src/app.js')]
   },
-  output: { publicPath: '/' },
+  output: { publicPath: isDevelopment ? '/' : '' },
   loaders: {
     svg: {
       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
