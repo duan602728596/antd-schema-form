eb9f0144d6ae41b1fd9e2e3d25f250d46931869b
段昊辰 <duanhaochen@126.com>
2018-12-27 21:42:42 +0800

    编写oneOf属性的测试用例

--- test/tests/object/test.js
+++ test/tests/object/test.js
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import React from 'react';
 import SchemaForm from '../../../src/SchemaForm';
+import styleName from '../../../src/utils/styleName';
 
 describe('组件渲染', function(): void{
   /* 组件渲染 */
@@ -57,6 +58,46 @@ describe('组件渲染', function(): void{
       expect(antInput).to.have.lengthOf(3);
       expect(antInputNumber).to.have.lengthOf(1);
       expect(antCheckboxInput).to.have.lengthOf(1);
+    });
+  });
+
+  /* oneOf渲染 */
+  describe('oneOf', function(): void{
+    it('oneOf', function(): void{
+      const json: Object = {
+        id: '$root',
+        title: 'oneOf',
+        oneOf: [
+          {
+            type: 'string'
+          },
+          {
+            type: 'number'
+          },
+          {
+            type: 'boolean'
+          }
+        ]
+      };
+
+      const wrapper: Object = mount(<SchemaForm json={ json } />);
+      const oneOfIndexBoxLi: Object = wrapper.find(`.${ styleName('oneOf-indexBox') }`).find('li');
+
+      // type 1
+      const antInput: Object = wrapper.find('.ant-input');
+      expect(antInput).to.have.lengthOf(1);
+
+      // type 2
+      oneOfIndexBoxLi.at(1).simulate('click');
+
+      const antInputNumberInput: Object = wrapper.find('.ant-input-number-input');
+      expect(antInputNumberInput).to.have.lengthOf(1);
+
+      // type 3
+      oneOfIndexBoxLi.at(2).simulate('click');
+
+      const antCheckbox: Object = wrapper.find('.ant-checkbox');
+      expect(antCheckbox).to.have.lengthOf(1);
     });
   });
 });
