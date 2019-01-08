cca9015d6ce7ea5a9325006adab865dd7d38af8c
duan602728596 <duanhaochen@126.com>
2019-01-08 17:39:54 +0800

    使用@babel/runtime

--- gulpfile.js
+++ gulpfile.js
@@ -26,10 +26,24 @@ const babelConfig = {
     '@babel/preset-flow'
   ],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true
+      }
+    ],
     '@babel/plugin-proposal-do-expressions',
     '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-class-properties'
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: true,
+        useESModules: true
+      }
+    ]
   ]
 };
 
