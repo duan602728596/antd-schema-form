2516db594151e6c0d5bc5e23ea463f87fcef8881
段昊辰 <duanhaochen@126.com>
2019-02-17 23:39:19 +0800

    refactor: state的默认值为undefined

--- src/components/FormArray/TableComponent.tsx
+++ src/components/FormArray/TableComponent.tsx
@@ -99,8 +99,8 @@ class TableComponent extends Component<TableComponentProps> {
     }
 
     this.setState({
-      inputDisplayIndex: null,
-      inputChangeIndex: null
+      inputDisplayIndex: undefined,
+      inputChangeIndex: undefined
     });
   }
 
