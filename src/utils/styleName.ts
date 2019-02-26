3dbcea80486f9b0a81b9fd3ec587d690f3d6bf76
duan602728596 <duanhaochen@126.com>
2019-02-26 14:03:22 +0800

    refactor: 调整字首变量的变量名称

--- src/utils/styleName.ts
+++ src/utils/styleName.ts
@@ -1,11 +1,11 @@
-const name: string = 'schema-form-';
+const prefix: string = 'schema-form-';
 
 /**
  * 返回一个类名
  * @param { string } className: 类名
  */
 function styleName(className: string): string {
-  return `${ name }${ className }`;
+  return `${ prefix }${ className }`;
 }
 
 export default styleName;
