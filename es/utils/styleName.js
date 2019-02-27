52d33b125cb965c5c453dffe18839a86a82e04cf
duan602728596 <duanhaochen@126.com>
2019-02-27 14:44:15 +0800

    chore: v2.0.0-beta.8

--- es/utils/styleName.js
+++
@@ -1,9 +0,0 @@
-const prefix = 'schema-form-';
-/**
- * 返回一个类名
- * @param { string } className: 类名
- */
-function styleName(className) {
-    return `${prefix}${className}`;
-}
-export default styleName;
