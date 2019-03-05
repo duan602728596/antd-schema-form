efd01fcabd23d37e6671af4d0a8d9111071bffc8
duan602728596 <duanhaochen@126.com>
2019-03-05 10:29:31 +0800

    fix: 解决可能获取不到oneOf内id的情况

--- src/utils/getKeysFromObject.ts
+++ src/utils/getKeysFromObject.ts
@@ -14,10 +14,32 @@ function getKeysFromObject(item: SchemaItem): Array<string> {
 
       idArr.push(...objIdArr);
     }
+
+    // 获取oneOf内的id
+    if (item.oneOf && item.oneOf.length > 0) {
+      for (const oneOfItem of item.oneOf) {
+        for (const key in oneOfItem.properties) {
+          const objIdArr: string[] = getKeysFromObject(oneOfItem.properties[key]);
+
+          objIdArr.forEach((objIdArrItem: string, index: number, array: Array<string>): void => {
+            if (!idArr.includes(objIdArrItem)) idArr.push(objIdArrItem);
+          });
+        }
+      }
+    }
   } else {
     const id: string = item.id;
 
     idArr.push(id);
+
+    // 获取oneOf内的id
+    if (item.oneOf && item.oneOf.length > 0) {
+      for (const oneOfItem of item.oneOf) {
+        const id: string | undefined = oneOfItem.id;
+
+        if (id && !idArr.includes(id)) idArr.push(id);
+      }
+    }
   }
 
   return idArr;
