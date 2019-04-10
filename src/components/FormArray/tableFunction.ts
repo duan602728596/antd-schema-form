5872b160fb16dc5a913375818f2b16c8641fdfc3
duan602728596 <duanhaochen@126.com>
2019-04-10 18:17:16 +0800

    refactor: 使用lodash优化函数

--- src/components/FormArray/tableFunction.ts
+++ src/components/FormArray/tableFunction.ts
@@ -1,15 +1,13 @@
+import { transform, sortBy } from 'lodash-es';
+
 /**
  * 格式化数据，使值满足antd的props-type
  * @param { Array<string | number> } rawArray: 原数据
  */
 export function formatTableValue(rawArray: Array<string | number>): Array<{ value: string | number }> {
-  const result: { value: string | number }[] = [];
-
-  for (const item of rawArray) {
+  return transform(rawArray, function(result: { value: string | number }[], item: string | number): void {
     result.push({ value: item });
-  }
-
-  return result;
+  }, []);
 }
 
 /**
@@ -18,20 +16,5 @@ export function formatTableValue(rawArray: Array<string | number>): Array<{ valu
 export function sortIndex(rawArray: Array<number>): Array<number> {
   if (rawArray.length <= 1) return rawArray;
 
-  for (let i: number = 0, j: number = rawArray.length; i < j; i++) {
-    let max: number = i;
-
-    for (let k: number = i + 1; k < j; k++) {
-      if (rawArray[k] > rawArray[max]) max = k;
-    }
-
-    if (max !== i) {
-      const middle: number = rawArray[max];
-
-      rawArray.splice(max, 1);
-      rawArray.splice(i, 0, middle);
-    }
-  }
-
-  return rawArray;
+  return sortBy(rawArray).reverse();
 }
