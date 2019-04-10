52f452dbc021655031c821100f66104353a468a0
duan602728596 <duanhaochen@126.com>
2019-04-10 14:49:58 +0800

    fix: 修复模块引入错误

--- src/modules/Preview/Layout.js
+++ src/modules/Preview/Layout.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import  from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import loadReducer from '../../store/loadReducer';
