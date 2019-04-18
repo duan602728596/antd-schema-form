46ba3efbf559ac3a810df40816cd51fbb384a034
段昊辰 <duanhaochen@126.com>
2019-04-19 00:49:45 +0800

    refactor: 添加版本号显示

--- src/assembly/Header/index.js
+++ src/assembly/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Icon, Select } from 'antd';
+import { version } from 'antd-schema-form/package.json';
 import style from './style.sass';
 import Nav from './Nav';
 import { I18NContext } from '../../components/I18N/I18N';
@@ -20,6 +21,7 @@ function Header(props) {
               )
             }
           </I18NContext.Consumer>
+          <span className={ style.version }>v{ version }</span>
           <a className={ style.github }
             href="https://github.com/duan602728596/antd-schema-form"
             target="_blank"
