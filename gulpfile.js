9bef736b6ce0f841395d20fc7ddbbd387d60f836
duan602728596 <duanhaochen@126.com>
2019-05-14 18:24:00 +0800

    ci: 添加d.ts的编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const typescript = require('gulp-typescript');
 const sass = require('gulp-sass');
+const merge = require('merge2');
 const tsconfig = require('./tsconfig.json');
 const tsconfigES5 = require('./tsconfig.es5.json');
 
 // 文件目录地址
 const tsSrc = 'src/**/*.{ts,tsx}';
+const dTsSrc = 'src/**/*.d.ts';
 const sassSrc = 'src/style/**/*.sass';
 const libPath = 'lib';
 const esPath = 'es';
@@ -17,7 +19,10 @@ function proLibProject() {
   const result = gulp.src(tsSrc)
     .pipe(typescript(tsconfigES5.compilerOptions));
 
-  return result.js.pipe(gulp.dest(libPath));
+  return merge([
+    result.js.pipe(gulp.dest(libPath)),
+    result.dts.pipe(gulp.dest(libPath))
+  ]);
 }
 
 /* es */
@@ -25,7 +30,17 @@ function proEsProject() {
   const result = gulp.src(tsSrc)
     .pipe(typescript(tsconfig.compilerOptions));
 
-  return result.js.pipe(gulp.dest(esPath));
+  return merge([
+    result.js.pipe(gulp.dest(esPath)),
+    result.dts.pipe(gulp.dest(esPath))
+  ]);
+}
+
+/* 拷贝d.ts */
+function copy() {
+  return gulp.src(dTsSrc)
+    .pipe(gulp.dest(libPath))
+    .pipe(gulp.dest(esPath));
 }
 
 /* sass */
@@ -37,4 +52,4 @@ function proSassProject() {
     .pipe(gulp.dest(stylePath));
 }
 
-exports.default = gulp.parallel(proLibProject, proEsProject, proSassProject);
+exports.default = gulp.parallel(proLibProject, proEsProject, proSassProject, copy);
