fb6ff8cabf037a785ead5ad6f257e118fb2d552d
duan602728596 <duanhaochen@126.com>
2019-06-12 17:45:44 +0800

    feat: 使用SFC重构string类型组件

--- src/components/FormString/FormString.tsx
+++ src/components/FormString/FormString.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Component, Context } from 'react';
+import { useContext, PropsWithChildren } from 'react';
 import * as PropTypes from 'prop-types';
-import { Requireable } from 'prop-types';
 import { isString } from 'lodash-es';
 import { Form, Tooltip, Input, Select, Radio, DatePicker } from 'antd';
 import { ValidationRule } from 'antd/lib/form';
@@ -10,7 +9,6 @@ import * as moment from 'moment';
 import AntdSchemaFormContext from '../../context';
 import styleName from '../../utils/styleName';
 import createStringRules from './createStringRules';
-import InputPassword from './InputPassword';
 import { StringItem, ContextValue } from '../../types';
 
 /**
@@ -26,112 +24,102 @@ interface FormStringProps {
   required: boolean;
 }
 
-class FormString extends Component<FormStringProps> {
-  static contextType: Context<ContextValue | {}> = AntdSchemaFormContext;
-  static propTypes: {
-    root: Requireable<object>;
-    required: Requireable<boolean>;
-  } = {
-    root: PropTypes.object,
-    required: PropTypes.bool
-  };
-
-  context: ContextValue;
+function FormString(props: PropsWithChildren<FormStringProps>): React.ReactElement | null {
+  const context: ContextValue | {} = useContext(AntdSchemaFormContext);
 
   // select的下拉框
-  selectOptionsView(options: Array<{ label: string; value: string }>): React.ReactNodeArray {
+  function selectOptionsView(options: Array<{ label: string; value: string }>): React.ReactNodeArray {
     return options.map((item: { label: string; value: string }, index: number): React.ReactNode => {
       return <Select.Option key={ `${ index }` } value={ item.value }>{ item.label }</Select.Option>;
     });
   }
 
-  render(): React.ReactNode {
-    const { form, customComponent }: ContextValue = this.context;
-    const { getFieldDecorator }: WrappedFormUtils = form;
-    // type=object时，会判断key是否存在于required数组中
-    const { root, required }: FormStringProps = this.props;
-    const {
-      id,
-      title,
-      description,
-      $required,
-      $componentType,
-      $readOnly,
-      $defaultValue,
-      $options = [],
-      $placeholder,
-      $hidden
-    }: StringItem = root;
-    const rules: Array<ValidationRule> = createStringRules(this.props.root, required);
-    const option: GetFieldDecoratorOptions = { rules };
-    let element: React.ReactNode = null;
+  if (!('form' in context)) return null; // 类型判断
 
-    // 表单默认值
-    if ($defaultValue) option.initialValue = $defaultValue;
+  const { form, customComponent }: ContextValue = context;
+  const { getFieldDecorator }: WrappedFormUtils = form;
+  const { root, required }: FormStringProps = props; // type=object时，会判断key是否存在于required数组中
+  const {
+    id,
+    title,
+    description,
+    $required,
+    $componentType,
+    $readOnly,
+    $defaultValue,
+    $options = [],
+    $placeholder,
+    $hidden
+  }: StringItem = root;
+  const rules: Array<ValidationRule> = createStringRules(props.root, required);
+  const option: GetFieldDecoratorOptions = { rules };
+  let element: React.ReactNode = null;
 
-    // 格式化日历的日期
-    if ($componentType === 'date' && isString($defaultValue)) {
-      option.initialValue = moment($defaultValue);
-    }
+  // 表单默认值
+  if ($defaultValue) option.initialValue = $defaultValue;
 
-    switch ($componentType) {
-      // 文本域
-      case 'textArea':
-        element = getFieldDecorator(id, option)(
-          <Input.TextArea rows={ 6 } readOnly={ $readOnly } placeholder={ $placeholder } />
-        );
-        break;
-
-      // 渲染select
-      case 'select':
-        element = getFieldDecorator(id, option)(
-          <Select className={ styleName('string-select') }
-            placeholder={ $placeholder }
-            allowClear={ !($required || required) }
-          >
-            { this.selectOptionsView($options) }
-          </Select>
-        );
-        break;
-
-      // 渲染radio
-      case 'radio':
-        element = getFieldDecorator(id, option)(<Radio.Group options={ $options } />);
-        break;
-
-      // 渲染日期组件
-      case 'date':
-        element = getFieldDecorator(id, option)(
-          <DatePicker format="YYYY-MM-DD HH:mm:ss" showTime={ true } placeholder={ $placeholder } />
-        );
-        break;
-
-      // password
-      case 'password':
-        element = getFieldDecorator(id, option)(
-          // 兼容Input.Password组件
-          'Password' in Input
-            ? <Input.Password readOnly={ $readOnly } placeholder={ $placeholder } />
-            : <InputPassword readOnly={ $readOnly } placeholder={ $placeholder } />
-        );
-        break;
-
-      // 渲染默认组件
-      default:
-        element = (customComponent && $componentType && $componentType in customComponent)
-          ? customComponent[$componentType](root, option, form, required)
-          : getFieldDecorator(id, option)(<Input readOnly={ $readOnly } placeholder={ $placeholder } />);
-        break;
-    }
-
-    return (
-      <Form.Item className={ $hidden ? styleName('hidden') : undefined } label={ title }>
-        <Tooltip title={ description } placement="topRight">
-          { element }
-        </Tooltip>
-      </Form.Item>
-    );
+  // 格式化日历的日期
+  if ($componentType === 'date' && isString($defaultValue)) {
+    option.initialValue = moment($defaultValue);
   }
+
+  switch ($componentType) {
+    // 文本域
+    case 'textArea':
+      element = getFieldDecorator(id, option)(
+        <Input.TextArea rows={ 6 } readOnly={ $readOnly } placeholder={ $placeholder } />
+      );
+      break;
+
+    // 渲染select
+    case 'select':
+      element = getFieldDecorator(id, option)(
+        <Select className={ styleName('string-select') }
+          placeholder={ $placeholder }
+          allowClear={ !($required || required) }
+        >
+          { selectOptionsView($options) }
+        </Select>
+      );
+      break;
+
+    // 渲染radio
+    case 'radio':
+      element = getFieldDecorator(id, option)(<Radio.Group options={ $options } />);
+      break;
+
+    // 渲染日期组件
+    case 'date':
+      element = getFieldDecorator(id, option)(
+        <DatePicker format="YYYY-MM-DD HH:mm:ss" showTime={ true } placeholder={ $placeholder } />
+      );
+      break;
+
+    // password
+    case 'password':
+      element = getFieldDecorator(id, option)(<Input.Password readOnly={ $readOnly } placeholder={ $placeholder } />);
+      break;
+
+    // 渲染默认组件
+    default:
+      element = (customComponent && $componentType && $componentType in customComponent)
+        ? customComponent[$componentType](root, option, form, required)
+        : getFieldDecorator(id, option)(<Input readOnly={ $readOnly } placeholder={ $placeholder } />);
+      break;
+  }
+
+  return (
+    <Form.Item className={ $hidden ? styleName('hidden') : undefined } label={ title }>
+      <Tooltip title={ description } placement="topRight">
+        { element }
+      </Tooltip>
+    </Form.Item>
+  );
 }
+
+FormString.propTypes = {
+  root: PropTypes.object,
+  required: PropTypes.bool
+};
 
 export default FormString;
