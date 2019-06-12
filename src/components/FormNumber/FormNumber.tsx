6070a3139afa7ef10bfbde0d20c56dddaaf92532
duan602728596 <duanhaochen@126.com>
2019-06-12 18:04:27 +0800

    feat: 使用SFC重构number类型组件

--- src/components/FormNumber/FormNumber.tsx
+++ src/components/FormNumber/FormNumber.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Component, Context } from 'react';
+import { useContext, PropsWithChildren } from 'react';
 import * as PropTypes from 'prop-types';
-import { Requireable } from 'prop-types';
 import { Form, Tooltip, InputNumber, Radio } from 'antd';
 import { ValidationRule } from 'antd/lib/form';
 import { GetFieldDecoratorOptions, WrappedFormUtils } from 'antd/lib/form/Form';
@@ -23,64 +22,59 @@ interface FormNumberProps {
   required: boolean;
 }
 
-class FormNumber extends Component<FormNumberProps> {
-  static contextType: Context<ContextValue | {}> = AntdSchemaFormContext;
-  static propTypes: {
-    root: Requireable<object>;
-    required: Requireable<boolean>;
-  } = {
-    root: PropTypes.object,
-    required: PropTypes.bool
-  };
+function FormNumber(props: PropsWithChildren<FormNumberProps>): React.ReactElement | null {
+  const context: ContextValue | {} = useContext(AntdSchemaFormContext);
 
-  context: ContextValue;
+  if (!('form' in context)) return null; // 类型判断
 
-  render(): React.ReactNode {
-    const { form, customComponent }: ContextValue = this.context;
-    const { getFieldDecorator }: WrappedFormUtils = form;
-    // type=object时，会判断key是否存在于required数组中
-    const { root, required }: FormNumberProps = this.props;
-    const {
-      id,
-      type,
-      title,
-      description,
-      $componentType,
-      $readOnly,
-      $defaultValue,
-      $options = [],
-      $placeholder,
-      $hidden
-    }: NumberItem = root;
-    const rules: Array<ValidationRule> = createNumberRules(this.props.root, required, type === 'integer');
-    const option: GetFieldDecoratorOptions = { rules };
-    let element: React.ReactNode = null;
+  const { form, customComponent }: ContextValue = context;
+  const { getFieldDecorator }: WrappedFormUtils = form;
+  const { root, required }: FormNumberProps = props; // type=object时，会判断key是否存在于required数组中
+  const {
+    id,
+    type,
+    title,
+    description,
+    $componentType,
+    $readOnly,
+    $defaultValue,
+    $options = [],
+    $placeholder,
+    $hidden
+  }: NumberItem = root;
+  const rules: Array<ValidationRule> = createNumberRules(props.root, required, type === 'integer');
+  const option: GetFieldDecoratorOptions = { rules };
+  let element: React.ReactNode = null;
 
-    // 表单默认值
-    if ($defaultValue) option.initialValue = $defaultValue;
+  // 表单默认值
+  if ($defaultValue) option.initialValue = $defaultValue;
 
-    switch ($componentType) {
-      // 渲染radio
-      case 'radio':
-        element = getFieldDecorator(id, option)(<Radio.Group options={ $options } />);
-        break;
+  switch ($componentType) {
+    // 渲染radio
+    case 'radio':
+      element = getFieldDecorator(id, option)(<Radio.Group options={ $options } />);
+      break;
 
-      default:
-        element = (customComponent && $componentType && $componentType in customComponent)
-          ? customComponent[$componentType](root, option, form, required)
-          : getFieldDecorator(id, option)(
-            <InputNumber className={ styleName('number-input') } readOnly={ $readOnly } placeholder={ $placeholder } />
-          );
-    }
-
-    return (
-      <Form.Item className={ $hidden ? styleName('hidden') : undefined } label={ title }>
-        <Tooltip title={ description } placement="topRight">
-          { element }
-        </Tooltip>
-      </Form.Item>
-    );
+    default:
+      element = (customComponent && $componentType && $componentType in customComponent)
+        ? customComponent[$componentType](root, option, form, required)
+        : getFieldDecorator(id, option)(
+          <InputNumber className={ styleName('number-input') } readOnly={ $readOnly } placeholder={ $placeholder } />
+        );
   }
+
+  return (
+    <Form.Item className={ $hidden ? styleName('hidden') : undefined } label={ title }>
+      <Tooltip title={ description } placement="topRight">
+        { element }
+      </Tooltip>
+    </Form.Item>
+  );
 }
+
+FormNumber.propTypes = {
+  root: PropTypes.object,
+  required: PropTypes.bool
+};
 
 export default FormNumber;
