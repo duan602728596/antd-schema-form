d82de813ea7d25ba05a238cfbc41d72d797582b0
duan602728596 <duanhaochen@126.com>
2019-07-02 17:38:47 +0800

    feat: 模板函数

---
+++ src/utils/template.ts
@@ -0,0 +1,14 @@
+/* 简单的template模板 */
+function template(tpl: string, data: { [key: string]: string | number }): string {
+  let result: string = tpl;
+
+  for (const key in data) {
+    const reg: RegExp = new RegExp(`{\\s*${ key }\\s*}`, 'g');
+
+    result = result.replace(reg, `${ data[key] }`);
+  }
+
+  return result;
+}
+
+export default template;
