1b366fd1cb940f3ae073f690e7ae7e39f39a19c9
段昊辰 <duanhaochen@126.com>
2019-07-12 19:45:50 +0800

    feat: 创建自定义属性

---
+++ src/customComponent.ts
@@ -0,0 +1,31 @@
+import {
+  defaultString,
+  textArea,
+  select,
+  radio,
+  date,
+  password,
+  defaultNumber,
+  booleanDefault,
+  switchComponent,
+  arrayDefault,
+  checkboxGroup,
+  multipleOrTags
+} from './widget/widget';
+
+const customComponent: object = {
+  defaultString,
+  textArea,
+  select,
+  radio,
+  date,
+  password,
+  defaultNumber,
+  booleanDefault,
+  switchComponent,
+  arrayDefault,
+  checkboxGroup,
+  multipleOrTags
+};
+
+export default customComponent;
