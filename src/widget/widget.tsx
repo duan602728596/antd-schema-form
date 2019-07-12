cc61642889fea3290cd87d20925a6d0be65fc6d3
段昊辰 <duanhaochen@126.com>
2019-07-12 19:41:52 +0800

    feat: 提取组件的创建方法

--- src/widget/widget.tsx
+++ src/widget/widget.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { Input, Select, Radio, DatePicker, InputNumber } from 'antd';
+import { Input, Select, Radio, DatePicker, InputNumber, Checkbox, Switch } from 'antd';
 import { GetFieldDecoratorOptions, WrappedFormUtils } from 'antd/lib/form/Form';
+import omit from 'lodash-es/omit';
 import selectOptionsRender from '../utils/selectOptionsRender';
 import styleName from '../utils/styleName';
-import { StringItem, NumberItem } from '../types';
+import TableComponent from '../components/FormArray/TableComponent';
+import { StringItem, NumberItem, BooleanItem, ArrayItem } from '../types';
 
 /* string类型组件 */
 // 默认组件
@@ -98,15 +100,88 @@ export function password(
 /* number类型组件 */
 // 默认组件
 export function defaultNumber(
-  root: StringItem,
+  root: NumberItem,
   option: GetFieldDecoratorOptions,
   form: WrappedFormUtils,
   required: boolean
 ): React.ReactNode {
   const { getFieldDecorator }: WrappedFormUtils = form;
-  const { id, $readOnly, $placeholder }: StringItem = root;
+  const { id, $readOnly, $placeholder }: NumberItem = root;
 
   return getFieldDecorator(id, option)(
     <InputNumber className={ styleName('number-input') } readOnly={ $readOnly } placeholder={ $placeholder } />
+  );
+}
+
+/* boolean类型组件 */
+// 默认组件
+export function booleanDefault(
+  root: BooleanItem,
+  option: GetFieldDecoratorOptions,
+  form: WrappedFormUtils,
+  required: boolean
+): React.ReactNode {
+  const { getFieldDecorator }: WrappedFormUtils = form;
+  const { id }: BooleanItem = root;
+
+  return getFieldDecorator(id, option)(<Checkbox />);
+}
+
+// switch组件
+export function switchComponent(
+  root: BooleanItem,
+  option: GetFieldDecoratorOptions,
+  form: WrappedFormUtils,
+  required: boolean
+): React.ReactNode {
+  const { getFieldDecorator }: WrappedFormUtils = form;
+  const { id }: BooleanItem = root;
+
+  return getFieldDecorator(id, option)(<Switch />);
+}
+
+/* Array类型组件 */
+// 默认组件
+export function arrayDefault(
+  root: ArrayItem,
+  option: GetFieldDecoratorOptions,
+  form: WrappedFormUtils,
+  required: boolean
+): React.ReactNode {
+  // @ts-ignore
+  const { getFieldProps }: WrappedFormUtils = form;
+  const { id }: ArrayItem = root;
+  const props: any = omit(getFieldProps(id, option), ['ref']);
+
+  return <TableComponent root={ root } { ...props } />;
+}
+
+// checkbox group
+export function checkboxGroup(
+  root: ArrayItem,
+  option: GetFieldDecoratorOptions,
+  form: WrappedFormUtils,
+  required: boolean
+): React.ReactNode {
+  const { getFieldDecorator }: WrappedFormUtils = form;
+  const { id, $options = [] }: ArrayItem = root;
+
+  return getFieldDecorator(id, option)(<Checkbox.Group options={ $options } />);
+}
+
+// multiple and tags
+export function multipleOrTags(
+  root: ArrayItem,
+  option: GetFieldDecoratorOptions,
+  form: WrappedFormUtils,
+  required: boolean
+): React.ReactNode {
+  const { getFieldDecorator }: WrappedFormUtils = form;
+  const { id, $options = [], $componentType }: ArrayItem = root;
+
+  return getFieldDecorator(id, option)(
+    <Select className={ styleName('array-multiple') } mode={ $componentType }>
+      { selectOptionsRender($options) }
+    </Select>
   );
 }
