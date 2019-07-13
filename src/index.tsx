f642954756de45d55cfa115fb5eecfd78c2dea44
段昊辰 <duanhaochen@126.com>
2019-07-13 09:23:57 +0800

    fix: 调整默认组件和自定义组件的合并顺序

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,10 @@ export default function(props: PropsWithChildren<SchemaFormProps>): React.ReactN
   const [custom, setCustom]: [
     object | undefined,
     Dispatch<SetStateAction<object>>
-  ] = useState(Object.assign(customComponent || {}, components));
+  ] = useState(Object.assign(components, customComponent || {}));
 
   useEffect(function(): void {
-    setCustom(Object.assign(customComponent || {}, components));
+    setCustom(Object.assign(components, customComponent || {}));
   }, [customComponent]);
 
   return <SchemaForm customComponent={ custom } { ...otherProps } />;
