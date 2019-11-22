8ab53b5a88c3d3f45b13c87ae2dcc6705b557d38
段昊辰 <duanhaochen@126.com>
2019-11-22 23:32:08 +0800

    fix: 还原valuePropName属性

--- src/components/FormBoolean/FormBoolean.tsx
+++ src/components/FormBoolean/FormBoolean.tsx
@@ -36,7 +36,11 @@ function FormBoolean(props: PropsWithChildren<FormBooleanProps>): ReactElement |
   }
 
   return element ? (
-    <Form.Item className={ $hidden ? styleName('hidden') : undefined } name={ id } label={ title }>
+    <Form.Item className={ $hidden ? styleName('hidden') : undefined }
+      name={ id }
+      label={ title }
+      valuePropName="checked"
+    >
       { element }
     </Form.Item>
   ) : null;
