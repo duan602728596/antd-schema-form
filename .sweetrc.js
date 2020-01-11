0906862073da730e4d9c954b72dbe0d3516aeddd
段昊辰 <duanhaochen@126.com>
2020-01-11 20:27:30 +0800

    build: 修改编译配置

--- .sweetrc.js
+++ .sweetrc.js
@@ -23,11 +23,6 @@ module.exports = {
     index: [path.join(__dirname, 'src/index.js')]
   },
   output: { publicPath: isDevelopment ? '/' : 'https://duan602728596.github.io/antd-schema-form/' },
-  resolve: {
-    alias: {
-      '@ant-design/icons/lib/dist$': path.join(__dirname, 'src/components/icons/icons')
-    }
-  },
   js: {
     plugins: [
       [
