0d0cc14eced2c79a53102eb5a1f6af2797be7d53
段昊辰 <duanhaochen@126.com>
2020-04-09 15:08:53 +0800

    fix: 修复了语言的判断

--- src/languagePack.ts
+++ src/languagePack.ts
@@ -5,7 +5,8 @@ import requireJson from './utils/requireJson';
 // 语言包，key值小写
 const languagePack: { [key: string]: object } = {
   'default': requireJson(defaultLang),
-  'zh-cn': requireJson(zhCNLang)
+  'zh-cn': requireJson(zhCNLang),
+  'zh-hans-cn': requireJson(zhCNLang)
 };
 
 export default languagePack;
