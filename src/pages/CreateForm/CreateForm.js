747c0963efb6f1677919a2905122f2efb5d760d8
段昊辰 <duanhaochen@126.com>
2021-02-24 17:00:37 +0800

    refactor: 调整context的使用

--- src/pages/CreateForm/CreateForm.js
+++ src/pages/CreateForm/CreateForm.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import style from './createForm.sass';
 import JsonInputTextarea from './JsonInputTextarea';
 import ChangeJson from './ChangeJson';
@@ -5,27 +6,20 @@ import { I18NContext } from '../../components/I18N/I18N';
 
 /* 表单创建 */
 function CreateForm(props) {
-  return (
-    <I18NContext.Consumer>
-      {
-        (context) => {
-          const { createForm } = context.languagePack;
+  const context = useContext(I18NContext);
+  const { createForm } = context.languagePack;
 
-          return [
-            <p key="introduction">{ createForm.introduction }</p>,
-            <div key="main" className={ style.flexBox }>
-              <div className={ style.flexLeftBox }>
-                <JsonInputTextarea />
-              </div>
-              <div className={ style.flexRightBox }>
-                <ChangeJson />
-              </div>
-            </div>
-          ];
-        }
-      }
-    </I18NContext.Consumer>
-  );
+  return [
+    <p key="introduction">{ createForm.introduction }</p>,
+    <div key="main" className={ style.flexBox }>
+      <div className={ style.flexLeftBox }>
+        <JsonInputTextarea />
+      </div>
+      <div className={ style.flexRightBox }>
+        <ChangeJson />
+      </div>
+    </div>
+  ];
 }
 
 export default CreateForm;
