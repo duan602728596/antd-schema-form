ff6a77ca6025aaa8ead388c6e5fc81bb702b4877
段昊辰 <duanhaochen@126.com>
2021-02-24 16:39:41 +0800

    refactor: 修改index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import { render } from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale-provider/zh_CN';
+import { storeFactory } from './store/store';
+import Layout from './layouts/Layout/index';
+import { I18N } from './components/I18N/I18N';
 
 /* app */
 render(
-  <App />,
+  <Provider store={ storeFactory() }>
+    <ConfigProvider locale={ zhCN }>
+      <I18N>
+        <HashRouter>
+          <Route element={ <Layout /> } />
+        </HashRouter>
+      </I18N>
+    </ConfigProvider>
+  </Provider>,
   document.getElementById('app')
 );
 
