0eae607b770db4c5f1525c3bb5b60f8d61402670
段昊辰 <duanhaochen@126.com>
2021-02-24 16:35:11 +0800

    ci: 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const { createBabelPlugins } = require('@sweet-milktea/milktea/lib/config/babelConfig');
-
 module.exports = {
   env: {
     browser: true,
@@ -22,7 +20,7 @@ module.exports = {
     },
     sourceType: 'module'
   },
-  plugins: ['react'],
+  plugins: ['react', 'import'],
   settings: {
     react: {
       version: 'detect'
@@ -180,12 +178,12 @@ module.exports = {
     'template-curly-spacing': ['error', 'always'], // 要求或禁止模板字符串中的嵌入表达式周围空格的使用
     // React
     'react/button-has-type': 'error',               // 禁止<button>元素没有显式的"type"属性
-    'react/prefer-es6-class': ['error', 'always'],  // 使用ES5创建反应类模块或新的ES6类系统
     'react/void-dom-elements-no-children': 'error', // 防止接收子节点的无效DOM元素（例如<img />，<br />）
     // JSX
-    'react/jsx-boolean-value': ['error', 'always'], // 在JSX中使用布尔属性时，可以将属性值设置为true或省略值
-    'react/jsx-closing-tag-location': 'error',      // 验证JSX中的结束标签位置
-    'react/jsx-curly-spacing': [                    // 在JSX属性和表达式中强制或禁止花括号内的空格
+    'react/jsx-boolean-value': ['error', 'always'],       // 在JSX中使用布尔属性时，可以将属性值设置为true或省略值
+    'react/jsx-closing-tag-location': 'error',            // 验证JSX中的结束标签位置
+    'react/jsx-curly-brace-presence': ['error', 'never'], // 强制使用大括号或不使用不必要的大括号
+    'react/jsx-curly-spacing': [ // 在JSX属性和表达式中强制或禁止花括号内的空格
       'error',
       {
         when: 'always',
@@ -195,17 +193,16 @@ module.exports = {
     'react/jsx-equals-spacing': 'error',                 // 强制或禁止JSX属性中的等号周围的空格
     'react/jsx-first-prop-new-line': ['error', 'never'], // 此规则检查所有JSX元素的第一个属性是否正确放置
     'react/jsx-indent': ['error', 2],                    // 验证JSX缩进
-    'react/jsx-indent-props': ['error', 2],              //  验证JSX中props的缩进
+    'react/jsx-indent-props': ['error', 2],              // 验证JSX中props的缩进
     'react/jsx-key': 'error',                            // 在数组或迭代器中验证JSX具有key属性
     'react/jsx-no-comment-textnodes': 'error',           // 防止将注释插入为文本节点
     'react/jsx-no-duplicate-props': 'error',             // 在JSX中防止重复的props
-    'react/jsx-no-target-blank': [ // 创建具有标记的JSX元素时，通常需要使用target='_ blank'属性在新选项卡中打开链接
+    'react/jsx-no-target-blank': [ // 创建具有标记的JSX元素时，通常需要使用target="_blank"属性在新选项卡中打开链接
       'error',
       { enforceDynamicLinks: 'never' }
     ],
-    'react/jsx-curly-brace-presence': ['error', 'never'], // 强制使用大括号或不使用不必要的大括号
-    'react/jsx-props-no-multi-spaces': 'error',           // 禁止内联JSX之间的多个空格
-    'react/jsx-tag-spacing': [                            // 验证JSX左右括号中的空格
+    'react/jsx-props-no-multi-spaces': 'error', // 禁止内联JSX之间的多个空格
+    'react/jsx-tag-spacing': [                  // 验证JSX左右括号中的空格
       'error',
       {
         closingSlash: 'never',
@@ -213,6 +210,11 @@ module.exports = {
         afterOpening: 'never',
         beforeClosing: 'never'
       }
+    ],
+    // import
+    'import/no-unresolved': [ // 确保导入的模块可以解析为本地文件系统上的模块
+      'error',
+      { commonjs: true }
     ]
   }
 };
