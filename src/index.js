e64b7997acc73fa4417cccf0afc33b08d81a314a
duan602728596 <duanhaochen@126.com>
2024-08-14 17:11:19 +0800

    fix: 修复样式问题

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import 'antd/dist/reset.css';
-
 import { storeFactory } from './store/store';
 import Layout from './layouts/Layout/index';
 import { I18N } from './components/I18N/I18N';
+import './index.global.sass';
 
 /* app */
 const root = createRoot(document.getElementById('app'));
